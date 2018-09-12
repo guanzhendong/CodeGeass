6303c5294e97b9a6a4df62e6c42c02449899289b
gzd <guanzd@ecqun.com>
2018-09-12 18:00:36 +0800

    URL scheme  APP跳转

--- CodeGeass/AppDelegate.m
+++ CodeGeass/AppDelegate.m
@@ -298,6 +298,17 @@
     [self.window.rootViewController presentViewController:nav animated:YES completion:nil];
 }
 
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+    if ([url.scheme isEqualToString:@"codegeass"]) {
+        NSString *backScheme = [url.absoluteString componentsSeparatedByString:@"back="].lastObject;
+        UIAlertController *alert = [UIAlertController zd_alertWithTitle:@"来自EC" message:@"点击确认返回EC" cancelButtonTitle:@"取消" clicked:^(NSUInteger buttonIndex) {
+            [[UIApplication sharedApplication] openURL:[NSURL URLWithString:[backScheme stringByAppendingString:@"://"]]];
+        } otherButtonTitles:@"确认", nil];
+        [self.window.rootViewController presentViewController:alert animated:YES completion:nil];
+    }
+    return NO;
+}
+
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
